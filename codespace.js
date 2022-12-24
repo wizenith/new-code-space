fef75b3bc39a7f0a46f889914501d473760afd54
wizenith <vorkibiz@gmail.com>
2022-12-24 09:05:46 +0000

    edited

--- codespace.js
+++ codespace.js
@@ -1,1 +1,5 @@
 console.log("log");
+// create a new instance of the CodeSpace class
+var cs = new CodeSpace();
+// add a new code block to the CodeSpace instance       
+cs.addCodeBlock("js", "console.log('hello world');", "hello world");
